b682dcc019b34a690d73365167177687f4ad0f9a
subha_py <subha.py@gmail.com>
2025-02-22 19:19:03 +0530

    adding parallel data upload

--- utils/connect.py
+++ utils/connect.py
@@ -5,10 +5,10 @@ def connect(host, port=27017):
     return client
 def get_db(client, db='test_database'):
     return client[db]
-def get_collection(db, collection='posts'):
-    return client[db][collection]
+def get_collection(client, db_name='test_database', collection='posts'):
+    return client[db_name][collection]
 
 if __name__ == '__main__':
     client = connect('10.3.59.156')
-    collection = get_collection(client)
+    collection = get_collection(client, )
     pprint(collection.find_one())
--- utils/insert.py
+++ utils/insert.py
@@ -3,7 +3,9 @@ import sys
 from string import ascii_letters
 import datetime
 from pprint import pprint
-
+from threading import Lock
+import concurrent.futures
+from utils.connect import get_collection, get_db, connect
 def create_random_doc():
     toggle = random.choice([True, False])
     task_number = random.randint(1, sys.maxsize)
@@ -19,13 +21,57 @@ def create_random_doc():
     return post
 
 
-def get_docs(batch_size):
+def create_random_docs(batch_size):
     docs = []
     for i in range(batch_size):
         docs.append(create_random_doc())
     return docs
 
+def process_batch(connection, batch_size,
+                batch_number,
+                lock, docs, number_of_batches):
+    if not docs:
+        docs = create_random_docs(batch_size)
+    collection = get_collection(connection)
+    print(f'{batch_number}/{number_of_batches}: inserting {batch_size} docs')
+    collection.insert_many(docs)
+    print(f'{batch_number}/{number_of_batches}: successfully inserted '
+          f'{batch_size} docs')
+    return
+def pump_data(connection, db_name=None, total_size=None, batch_size=100,
+                  max_threads=128):
+    total_docs_required = 10000 #todo
+    number_of_batches = total_docs_required // batch_size
+    future_to_batch = {}
+    workers = min(max_threads, number_of_batches)
+    print('number of workers - {}'.format(workers))
+    lock = Lock()
+    with concurrent.futures.ThreadPoolExecutor(
+            max_workers=workers) as executor:
+        for batch_number in range(1, number_of_batches + 1):
+            arg = (
+                connection, batch_size,
+                batch_number,
+                lock, None, number_of_batches)
+            future_to_batch[
+                executor.submit(process_batch, *arg)] = batch_number
+
+    result = []
+    for future in concurrent.futures.as_completed(future_to_batch):
+        batch_number = future_to_batch[future]
+        try:
+            res = future.result()
+            if not res:
+                result.append(batch_number)
+        except Exception as exc:
+            print("%r generated an exception: %s" % (batch_number, exc))
+            # todo: handle here sequentially for error batches
+
+    return result
+
+
+
 
 if __name__ == '__main__':
-    docs = get_docs(3)
-    pprint(docs)
+    connection = connect('10.3.59.156')
+    pump_data(connection)
