9e819dd84c09004b9966e14292192f5a46145426
subha_py <subha.py@gmail.com>
2025-03-16 17:00:40 +0530

    changes

--- utils/insert.py
+++ utils/insert.py
@@ -5,7 +5,8 @@ import datetime
 import concurrent.futures
 from utils.connect import get_collection, connect, create_databases
 from utils.memory import get_number_of_rows_from_size
-
+sys.path.append('/root/mongodb_pumper/utils')
+sys.path.append('/root/mongodb_pumper')
 def create_random_doc():
     toggle = random.choice([True, False])
     task_number = random.randint(1, sys.maxsize)
@@ -27,11 +28,12 @@ def create_random_docs(batch_size):
         docs.append(create_random_doc())
     return docs
 
-def process_batch(connection, batch_size,
+def process_batch(ip, batch_size,
                 batch_number,
                 docs, number_of_batches, db_name, collection_name):
     if not docs:
         docs = create_random_docs(batch_size)
+    connection = connect(ip)
     collection = get_collection(connection, db_name, collection_name)
     print(f'{batch_number}/{number_of_batches}: inserting {batch_size} docs')
 
@@ -39,11 +41,12 @@ def process_batch(connection, batch_size,
     print(f'{batch_number}/{number_of_batches}: successfully inserted '
           f'{batch_size} docs')
     return
-def pump_data(connection, total_size, batch_size=10000,
+def pump_data(ip, total_size, batch_size=10000,
                   max_threads=128, create_database=False):
     number_of_batches = get_number_of_rows_from_size(total_size) // batch_size
     future_to_batch = {}
     workers = min(max_threads, number_of_batches)
+    connection = connect(ip)
     if create_database:
         create_databases(connection)
     dbs = []
@@ -56,7 +59,7 @@ def pump_data(connection, total_size, batch_size=10000,
             max_workers=workers) as executor:
         for batch_number in range(1, number_of_batches + 1):
             arg = (
-                connection, batch_size,
+                ip, batch_size,
                 batch_number, None, number_of_batches, random.choice(dbs),
                 random.choice(collections))
             future_to_batch[
@@ -74,5 +77,6 @@ def pump_data(connection, total_size, batch_size=10000,
     return result
 
 if __name__ == '__main__':
-    connection = connect('10.3.59.157')
-    pump_data(connection, '100M', create_database=True)
+    # connection = connect('10.3.59.157')
+    ip = '10.3.59.157'
+    pump_data(ip, '2T', create_database=False)
