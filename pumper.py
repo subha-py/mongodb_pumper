6703109c77a4a250129c9e10a0ff18c49f73a241
subha_py <subha.py@gmail.com>
2025-05-06 12:38:38 +0530

    added argparse in pumper

--- pumper.py
+++ pumper.py
@@ -1,10 +1,25 @@
 import argparse
+from utils.insert import pump_data
+from utils.connect import connect
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='A program to populate dbs in mongodb',
-        usage='python3 pumper.py --host 10.14.69.121 --db_name prodsb21\
-                        --user sys --password cohesity --total_size 1G \
-                        --datafile_size 200M --batch_size 200000 ')
+        usage='python3 pumper.py --host st-monog-1')
     parser._action_groups.pop()
     required = parser.add_argument_group('required arguments')
     optional = parser.add_argument_group('optional arguments')
+
+    # add arguments here
+    required.add_argument('--host', help='ip/hostname of the db', type=str, required=True)
+    optional.add_argument('--size', help='size of data will be inserted(default 100G)', type=str, default='100G')
+    optional.add_argument('--batch_size', help='number of rows to inserted at a time', type=int, default=10000)
+    optional.add_argument('--threads', help='threads to run data pump', type=int, default=128)
+
+    # parsing
+    result = parser.parse_args()
+
+    # starting data pump
+    connection = connect(result.host)
+    pump_data(connection=connection, total_size=result.size,batch_size=result.batch_size,max_threads=result.threads,
+        create_database=True)
